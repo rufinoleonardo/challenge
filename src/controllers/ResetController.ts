4a73a875922d5d9da5a94df85df975da27ef75b1
Leonardo <rufinoleonardo@outlook.com>
2024-05-10 20:29:39 -0300

    Final code. Tests went well

--- src/controllers/ResetController.ts
+++ src/controllers/ResetController.ts
@@ -7,7 +7,7 @@ const resetRouter = Router();
 resetRouter.post("/", (req: Request, res: Response) => {
     // TODO: zerar o Objeto instanciado.
     eventsServices.Reset();
-    res.status(200).send("Ok")
+    res.status(200).send("OK")
 })
 
 export { resetRouter };
